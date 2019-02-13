477379dc81643cd36a5985944461aa7978ef52d9
tan <haitanle88@gmail.com>
2019-02-13 10:40:18 -0500

    make ajax request with jquery to unsplash

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -1,14 +1,47 @@
-/* eslint-env jquery */
-
 (function () {
+
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
+    let searchedForText = '';
     const responseContainer = document.querySelector('#response-container');
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
-    });
+
+        $.ajax({
+        	url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+        	headers: {'Authorization': 
+        		'Client-ID c9c7834c3c62289626fe85278787c56b16cd81d2c38a2c0675e234290ac0a8c2'}
+
+        	}).done(addImage);
+
+
+
+        });
+
+        // const unsplashRequest = new XMLHttpRequest();
+        // unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        // unsplashRequest.setRequestHeader('Authorization', 'Client-ID c9c7834c3c62289626fe85278787c56b16cd81d2c38a2c0675e234290ac0a8c2');
+
+        // unsplashRequest.onload = addImage;
+        // unsplashRequest.send();
+
+    function addImage(data) {
+        let htmlContent = '';
+
+        if (data && data.results && data.results[0]) {
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+        	<img src="${firstImage.urls.regular}" alt="${searchedForText}">
+        	<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        	</figure>`;
+        } else {
+            htmlContent = '<div class="error-no-image">No images available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
 })();
