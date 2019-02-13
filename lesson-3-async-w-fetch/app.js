4d3b7fdc3558765a9efafec1ce3cfaa6ed38c7a1
tan <haitanle88@gmail.com>
2019-02-13 11:51:50 -0500

    make async request using fetch to unsplash

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,32 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+		{
+			headers: {'Authorization':
+			'Client-ID c9c7834c3c62289626fe85278787c56b16cd81d2c38a2c0675e234290ac0a8c2'}
+		}).then(function(response){
+			return response.json();
+		}).then(addImage)
+
+
+	function addImage(data) {
+		debugger
+	let htmlContent = '';
+
+	if (data && data.results && data.results[0]) {
+	    const firstImage = data.results[0];
+	    htmlContent = `<figure>
+		<img src="${firstImage.urls.regular}" alt="${searchedForText}">
+		<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+		</figure>`;
+	} else {
+	    htmlContent = '<div class="error-no-image">No images available</div>';
+	}
+
+	responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+	}	
+
     });
 })();
